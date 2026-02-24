477197a55cd77bf0e99ceb7d482517fd6b3135bf
Anna Chentoroh <annachentoroh@gmail.com>
2026-02-24 22:38:10 +0200

    Creating 3 functions for input.py and 2 functions for output.py

---
+++ app/io/input.py
@@ -0,0 +1,6 @@
+def input_text_from_console():
+    pass
+def read_from_file_pythonbuiltin():
+    pass
+def read_from_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,4 @@
+def output_text_to_console():
+    pass
+def write_text_to_pythonbuiltin():
+    pass
